49b921de23bf42f560ac4ebad2f5143051df50bb
Ernesto Avila Domenech <eadomenech@gmail.com>
2018-11-10 16:36:56 -0500

    Pudiendo gestionar los nuevos modelos desde el admin

--- watermarking/admin.py
+++ watermarking/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from .models import Watermarking, CoverImage, WatermarkImage, Noise, Metric
+from .models import (
+    Watermarking, CoverImage, WatermarkImage, Noise, Metric, SprintWatermarking,
+    MetricSprintWatermarking, NoiseSprintWatermarking,
+    MetricNoiseSprintWatermarking)
 
 admin.site.register(Watermarking)
 admin.site.register(CoverImage)
 admin.site.register(WatermarkImage)
 admin.site.register(Noise)
 admin.site.register(Metric)
+admin.site.register(SprintWatermarking)
+admin.site.register(MetricSprintWatermarking)
+admin.site.register(NoiseSprintWatermarking)
+admin.site.register(MetricNoiseSprintWatermarking)
